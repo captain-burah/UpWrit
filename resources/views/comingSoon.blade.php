9f679f566823609e8f05096be7c85a1b7fead46f
captain_burah <cptburah@gmail.com>
2021-06-26 15:54:24 +0530

    commit

--- resources/views/comingSoon.blade.php
+++ resources/views/comingSoon.blade.php
@@ -2,8 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
 
+        <link rel="icon" type="image/png" href="{{asset('img/favicon/5.png')}}">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script> window.laravel = { csrfToken: '{{ csrf_token() }}' } </script>
         <title>UpWrit Solutions</title>
         
         <!-- Font Awesome icons (free version)-->
@@ -13,7 +19,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Concert+One&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        
     </head>
 
     
@@ -21,6 +27,6 @@
         <div id="app">
             <comingsoon></comingsoon>
         </div>
-
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
 </html>
