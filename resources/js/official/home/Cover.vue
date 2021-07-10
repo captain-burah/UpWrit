ae9ca858070ee208e120175ba3db9ca0d78bdec0
captain_burah <cptburah@gmail.com>
2021-07-10 14:43:18 +0530

    Official | Carousel Contents RnD

--- resources/js/official/home/Cover.vue
+++ resources/js/official/home/Cover.vue
@@ -3,34 +3,74 @@
     
     <div class="row-span-5 grid row md:grid-cols-5 my-auto flex mx-auto px-0 md:pt-5">
         
-        <div class="md:col-span-2 flex"> 
+        <div class="md:col-span-3 flex w-full"> 
             
             <div  
-                class="my-auto md:px-24 md:py-8 px-8 pt-8 pb-4 mx-auto space-y-4 ">
+                class=" w-full my-auto md:pl-16 md:pr-8 md:py-8 px-8 pt-8 pb-4 mx-auto space-y-4 ">
                 
-                <div class="md:max-w-xl max-w-md">
+                <div class="w-full">
 
                     <carousel :autoplay="true" :nav="false" :items="1" :margin="20" :dots="false"
-                        :responsive="{0:{items:1,nav:false},600:{items:3,nav:true}}">
-                        <div>
-                            <h1 class="md:text-6xl text-6xl text-indigo-400 font-bold mb-4">
-                                Headline comes here!
-                            </h1>
-                            <p class="text-gray-600 md:text-2xl text-xl text-justify">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        :responsive="{0:{items:1,nav:false},600:{items:3,nav:true}}"
+                        class="md:max-w-full max-w-md">
+                        <div >
+                            <p class="text-gray-400 md:text-xl text-lg text-left pl-4 pb-2">
+                                We Make IT Happen.
                             </p>
+                            <div class="border-l-4 border-pink-400 pl-4 pr-4 pb-4 pt-1 ">
+                                
+                                <h1 class="md:text-7xl text-6xl text-gray-600 font-bold pb-4">
+                                    Where do you want to go today? 
+                                </h1>
+                                <p class="text-gray-500 md:text-2xl text-xl pt-4 text-justify">
+                                    We at UpWrit, deliver innovations that drive stronger administration, management and support our clients need.
+                                </p>
+                            </div>
                         </div>
                         <div>
-                            <h1 class="md:text-6xl text-6xl text-red-400 font-bold mb-4">
-                                Headline comes Two!
-                            </h1>
-                            <p class="text-gray-600 md:text-2xl text-xl text-justify">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            <p class="text-gray-400 md:text-xl text-lg text-left pl-4 pb-2">
+                                Your potential. Our passion.
                             </p>
+                            <div class="border-l-4 border-blue-400 pl-4 pr-4 pb-4 pt-1 ">
+                                <h1 class="md:text-7xl text-6xl text-gray-600 font-bold pb-4">
+                                    Are you looking for a Brand recognition?
+                                </h1>
+                                <p class="text-gray-500 md:text-2xl text-xl pt-4 text-justify">
+                                    Sometimes, the best laid-out plans go awry and you tend to wonder - why?
+                                    <br>
+                                    That is precisely what we deliver, a driving force creating, designing 
+                                    and developing your needs to the extent of first success.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="text-gray-400 md:text-xl text-lg text-left pl-4 pb-2">
+                                Can You Hear Me Now? Good.
+                            </p>
+                            <div class="border-l-4 border-blue-400 pl-4 pr-4 pb-4 pt-1 ">
+                                <h1 class="md:text-7xl text-6xl text-gray-600 font-bold pb-4">
+                                    Want to take your Business online?
+                                </h1>
+                                <p class="text-gray-500 md:text-2xl text-xl pt-4 text-justify">
+                                    Get yourself a customized, user friendly and responsive website by defining your standards, to be followed
+                                    and craft a unique intuitive digital experience for your business.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="text-gray-400 md:text-xl text-lg text-left pl-4 pb-2">
+                                A Virtual World of Live Pictures.
+                            </p>
+                            <div class="border-l-4 border-blue-400 pl-4 pr-4 pb-4 pt-1 ">
+                                <h1 class="md:text-7xl text-6xl text-gray-600 font-bold pb-4">
+                                    Yes, we design all Graphical contents.
+                                </h1>
+                                <p class="text-gray-500 md:text-2xl text-xl pt-4 text-justify">
+                                    When you turn to craft a face for a brand or product, you need someone who can 
+                                    visualize the unpredictable and that's where we come in to play, narrating stories through simple strokes 
+                                    of brush and paint in digital space.
+                                </p>
+                            </div>
                         </div>
                     </carousel>    
                 </div>
@@ -39,7 +79,7 @@
 
         </div>
 
-        <div class="hidden relative md:block md:col-span-3 md:max-w-full px-8">
+        <div class="hidden relative md:block md:col-span-2 md:max-w-full px-8">
             <div 
                 class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 
                     shadow-2xl transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl w-3/4 mx-auto">
