cd30dc5375797b4016ccbcf63c14b52b5d74a52a
captain_burah <cptburah@gmail.com>
2021-11-03 00:47:56 +0530

    links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,12 +21,12 @@
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet"> 
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet">
         
-        <link rel="stylesheet" type="text/css" href="owlcarousel/assets/owl.carousel.min.css">
-        <link rel="stylesheet" type="text/css" href="owlcarousel/assets/owl.theme.default.min.css">
-        <link href="https://unpkg.com/aos@next/dist/aos.css" rel="stylesheet">
-        <link href="css/app.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{{ ('owlcarousel/assets/owl.carousel.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ ('owlcarousel/assets/owl.theme.default.min.css') }}">
+        <link href="{{ ('https://unpkg.com/aos@next/dist/aos.css') }}" rel="stylesheet">
+        <link href="{{ ('css/app.css') }}" rel="stylesheet">
         
-        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <script src="{{ ('https://use.fontawesome.com/releases/v5.15.3/js/all.js') }}"></script>
         <style>
            
         </style>
@@ -35,10 +35,10 @@
         <div id="app" class="">
             <index></index>
         </div>
-        <script src="js/app.js" defer></script>
-        <script src="owlcarousel/jquery.min.js"></script>
-        <script src="owlcarousel/owl.carousel.min.js"></script>
-        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('owlcarousel/jquery.min.js') }}"></script>
+        <script src="{{ ('owlcarousel/owl.carousel.min.js') }}"></script>
+        <script src="{{ ('https://unpkg.com/aos@next/dist/aos.js') }}"></script>
         <script>
           AOS.init();
         </script>
