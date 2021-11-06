d441e64c134562c1cc4ceb933c60fcf948a68dcc
captain_burah <cptburah@gmail.com>
2021-11-07 00:02:38 +0530

    meta data | SEO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,47 @@
 <!DOCTYPE html>
 <html>
     <head>
+
+        <!-- Standard Meta Descriptions -->
         <meta charset="utf-8" />
+        <meta name="description" content="We'll help you to navigate through today's 
+            evolutionized business environments providing a wide range of services for you & your business
+            specialized by young & enthusiastic professionals on-board ready to kick-off.">
+        <meta name="keywords" content="IT, Business, Solutions, Web, Design, Marketing, Writers">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+        <!-- OpenGraph Meta Descriptions -->
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:title" content="UpWrit Solutions | Your Premium Tailor in Business Solutions" />
+        <meta property="og:site_name" content="UpWrit Solutions" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.upwritsolutions.com" />
+        <meta property="og:description" content="We'll help you to navigate through today's 
+            evolutionized business environments providing a wide range of services for you & your business
+            specialized by young & enthusiastic professionals on-board ready to kick-off." />
+
+        <!-- Twitter Meta Descriptions -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:description" content="We'll help you to navigate through today's 
+            evolutionized business environments providing a wide range of services for you & your business
+            specialized by young & enthusiastic professionals on-board ready to kick-off." />
+        <meta name="twitter:title" content="UpWrit Solutions | Your Premium Tailor in Business Solutions">
+        <meta name="twitter:image" content="img/v2/meta/UpWrit.png">
+
+        <!-- Geo Meta Descriptions -->
+        <meta name="geo.placename" content="Colombo">
+        <meta name="geo.region" content="LK">
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <script> window.laravel = { csrfToken: '{{ csrf_token() }}' } </script>
         <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
 
-        <title>UpWrit Solutions</title>
+        <link rel="dns-prefetch" href="//google.com">
+        <link rel="canonical" href="http://upwritsolutions.com/">
+        
+        <link rel="icon" href="img/v2/meta/upwrit-(logo-web-100x100).ico" type="image/x-icon" sizes="16x16"> 
+        <title>UpWrit Solutions | Your Premium Tailor in Business Solutions</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -26,10 +58,8 @@
         <link href="https://unpkg.com/aos@next/dist/aos.css" rel="stylesheet">
         <link href="css/app.css" rel="stylesheet">  
         
+        <!-- Script -->
         <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin></script>
-        <style>
-           
-        </style>
     </head>
     <body>
         <div id="app" class="">
